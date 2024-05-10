e66acf9c2b7090a68b5e9f16ffb7b6d98dd29235
zzzSeMzzz <zzzsemzzz@bk.ru>
2024-05-10 11:12:49 +0300

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+import com.android.tools.r8.internal.he
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.playmusic4"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.playmusic4"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.ka.helpers)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/playmusic4/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.playmusic4
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import dev.funkymuse.viewbinding.viewBinding
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import com.example.playmusic4.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val DEFAULT_URL = "https://playmusic23.com"
+    }
+
+    private val vb by viewBinding(ActivityMainBinding::inflate)
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(vb.root)
+
+        vb.webView.webViewClient = WebViewClient()
+        with(vb.webView.settings) {
+            javaScriptEnabled = true
+        }
+
+        val path = if (intent?.action == Intent.ACTION_VIEW) {
+            val appLink: Uri? = intent.data
+            Log.d(TAG, "onCreate: onNewIntent: $appLink")
+            appLink.toString().replace("intent://", "https://")
+        } else {
+            DEFAULT_URL
+        }
+
+        vb.webView.loadUrl(path)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d(TAG, "onNewIntent: ${intent.data}")
+        intent.data?.let {
+            vb.webView.loadUrl(it.toString().replace("intent://", "https://"))
+        }
+    }
+
+
+}
