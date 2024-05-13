a2103a9715fc99cab8893cb8aafb3b9e0dfbee11
zzzSeMzzz <zzzSeMzzz@bk.ru>
2024-05-13 15:51:27 +0300

    support url

--- app/src/main/java/com/example/playmusic4/MainActivity.kt
+++ app/src/main/java/com/example/playmusic4/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playmusic4
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -8,8 +9,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playmusic4.databinding.ActivityMainBinding
 import com.example.playmusic4.util.JsInterface
@@ -38,7 +41,31 @@ class MainActivity : AppCompatActivity() {
 
         wv = vb.webView
 
-        vb.webView.webViewClient = WebViewClient()
+        vb.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url?.toString().toString()
+                val urlU = request?.url
+
+                if (url.contains("t.me") || url.contains("tg://") ||
+                    url.contains("vk.com") || url.contains("www.youtube.com")) {
+                    try {
+                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                    } catch (e: ActivityNotFoundException) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Такого приложения нет на вашем телефоне",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
         vb.webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
         vb.webView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
         vb.webView.addJavascriptInterface(JsInterface(this), "JSOUT")
