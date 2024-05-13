7a1d0665738defcaeac34d6921de8bbef1fb3e9d
zzzSeMzzz <zzzSeMzzz@bk.ru>
2024-05-13 15:43:55 +0300

    add js interface

--- app/src/main/java/com/example/playmusic4/MainActivity.kt
+++ app/src/main/java/com/example/playmusic4/MainActivity.kt
@@ -6,12 +6,15 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import dev.funkymuse.viewbinding.viewBinding
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import com.example.playmusic4.databinding.ActivityMainBinding
+import com.example.playmusic4.util.JsInterface
 
 
 class MainActivity : AppCompatActivity() {
@@ -37,8 +40,64 @@ class MainActivity : AppCompatActivity() {
         wv = vb.webView
 
         vb.webView.webViewClient = WebViewClient()
+        vb.webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+        vb.webView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        vb.webView.addJavascriptInterface(JsInterface(this), "JSOUT")
         with(vb.webView.settings) {
             javaScriptEnabled = true
+            loadsImagesAutomatically = true
+            domStorageEnabled = true
+        }
+
+
+        vb.webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                if (newProgress == 100) {
+                    val code = "var mediaElement;" +
+                            "mediaCheck();" +
+                            "document.onclick = function(){" +
+                            "    mediaCheck();" +
+                            "};" +
+                            "function mediaCheck(){" +
+                            "    for(var i = 0; i < document.getElementsByTagName('video').length; i++){" +
+                            "        var media = document.getElementsByTagName('video')[i];" +
+                            "        media.onplay = function(){" +
+                            "            mediaElement = media;" +
+                            "            JSOUT.mediaAction('true');" +
+                            "        };" +
+                            "        media.onpause = function(){" +
+                            "            mediaElement = media;" +
+                            "            JSOUT.mediaAction('false');" +
+                            "        };" +
+                            "    }" +
+                            "    for(var i = 0; i < document.getElementsByTagName('audio').length; i++){" +
+                            "        var media = document.getElementsByTagName('audio')[i];" +
+                            "        media.onplay = function(){" +
+                            "            mediaElement = media;" +
+                            "            JSOUT.mediaAction('true');" +
+                            "        };" +
+                            "        media.onpause = function(){" +
+                            "            mediaElement = media;" +
+                            "            JSOUT.mediaAction('false');" +
+                            "        };" +
+                            "    }" +
+                            "}"
+                    vb.webView.evaluateJavascript(code, null)
+                }
+            }
+
+            override fun onReceivedIcon(view: WebView?, icon: Bitmap?) {
+                super.onReceivedIcon(view, icon)
+                img = icon!!
+
+
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                super.onReceivedTitle(view, title)
+                webTitle = title!!
+            }
         }
 
         val path = if (intent?.action == Intent.ACTION_VIEW) {
